a94f23aa7a4135046e2b0b87860ab57e4310f800
axi_e <axi_e@MSI>
2016-05-11 14:55:25 +0200

    Cosas que faltan (Cliente):
    - Afinar ServiceLocator(el tema del nombre)
    - GUI

--- TraLaLa_Client/src/controller/Busc_Add_Controller.java
+++ TraLaLa_Client/src/controller/Busc_Add_Controller.java
@@ -1,5 +1,16 @@
 package controller;
 
 public class Busc_Add_Controller {
+	
+	private TraServiceLocator tsl;
+	
+	public Busc_Add_Controller(){ // Poner args como parametro o no? para que?
+		// inicializar el ServiceLocator
+		this.tsl = new TraServiceLocator(); // Tendremos que ver lo que le pasamos y lo que hacemos en su constructor
+	}
+	
+	// Los metodos que se necesiten para responder a los eventos del GUI
+	
+	
 
 }
--- TraLaLa_Client/src/remote/TraServiceLocator.java
+++ TraLaLa_Client/src/remote/TraServiceLocator.java
@@ -1,5 +1,38 @@
 package remote;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class TraServiceLocator {
+	
+	private IGlobalService service; // Da error porque falta el lib
+	
+	public TraServiceLocator(){
+		// Constructor vacio
+	}
+	
+	public void setService(){
+		// Inicializar el façade global. se hara una sola vez
+		String name; // Este nombre en funcion de que?
+		try{
+			service = (IGlobalService) java.rmi.Naming.lookup(name);
+			
+		}
+		catch(MalformedURLException | RemoteException | NotBoundException e){
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public IGlobalService getService(){
+		// Cada vez que se quiera acceder al servidor, se tendra que hacer esto
+		
+		return service;
+	}
+	
+	
 
 }
